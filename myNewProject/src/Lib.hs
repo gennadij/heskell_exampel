1716a4f6a2049a9b9a5d37e95fc3c25050435faf
Gennadi Heimann <genaheimann@gmail.com>
2020-11-03 23:01:23 +0100

    impl

--- myNewProject/src/Lib.hs
+++ myNewProject/src/Lib.hs
@@ -12,15 +12,20 @@ calcOdd 0 = []
 calcOdd x = filter odd [1 .. x]
 
 -- berechne alle moegliche Wurzeln bis zu radicand
-
--- 1 + 2 :
-
+-- radicand -> Liste ungerade Zahlen -> Liste mit Wurzeln
 calcStandartRoots :: Int -> [Int] -> [Int]
 calcStandartRoots radicand (x:y:xs)
-    | summe < radicand = summe : calcStandartRoots radicand (summe : xs)
+    | summe <= radicand = summe : calcStandartRoots radicand (summe : xs)
     | otherwise        = []
     where summe = x + y
+-- suche den radicand in der Liste Standardswurzeln
 
-test :: Int -> [Int]
-test radicand = calcStandartRoots radicand (calcOdd radicand)
+simpleSerchInStandartRoots :: Int -> [Int] -> [Int]
+simpleSerchInStandartRoots radicand [] = []
+simpleSerchInStandartRoots radicand xs = filter (\x -> radicand == x) xs 
+
+test1 :: Int -> [Int]
+test1 radicand = calcStandartRoots radicand (calcOdd radicand)
+test2 :: Int -> [Int]
+test2 radicand = simpleSerchInStandartRoots radicand (calcStandartRoots radicand (calcOdd radicand))
 
