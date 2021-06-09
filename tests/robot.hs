0c6dae270dcda41a2eaa9eb53ec4f78ce852a9a5
Gennadi Heimann <genaheimann@gmail.com>
2021-06-09 13:11:00 +0000

    robot

---
+++ tests/robot.hs
@@ -0,0 +1,29 @@
+robot (name, attack, hp) = \msg -> msg (name, attack, hp)
+
+name (n, _, _) = n
+attack (_, a, _) = a
+hp (_, _, hp) = hp
+
+getName aRobot = aRobot name
+getAttack aRobot = aRobot attack
+getHp aRobot = aRobot hp
+
+setName aRobot newName = aRobot (\(n, a, h) -> (newName, a, h))
+setAttack aRobot newAttack = aRobot (\(n,a,h) -> robot (n,newAttack,h))
+setHP aRobot newHP = aRobot (\(n,a,h) -> robot (n,a,newHP))
+
+printRobot aRobot = aRobot (\(n,a,h) -> n ++
+  " attack:" ++ (show a) ++ " hp:"++ (show h))
+
+damage aRobot attackDamage = aRobot (\(n,a,h) ->
+  robot (n,a,h-attackDamage))
+
+fight aRobot defender = damage defender attack
+  where attack = if getHP aRobot > 10
+                 then getAttack aRobot
+                 else 0
+
+
+
+
+
