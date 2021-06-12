090c3ad85b5c8da2dc74a12ebca440224f3d3883
Gennadi Heimann <genaheimann@gmail.com>
2021-06-12 21:28:35 +0000

    rot13

--- tests/rot13.hs
+++ tests/rot13.hs
@@ -13,4 +13,10 @@ rotChar :: Char -> Char
 rotChar charToEncrypt = rotN sizeOfAlphabet charToEncrypt
   where sizeOfAlphabet = 1 + fromEnum (maxBound :: Char)
 
+message :: [FourLetterAlphabet]
+message = [L1,L3,L4,L1,L1,L2]
 
+fourLetterAlphabetEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder vals = map rot4l vals
+  where alphaSize = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rot4l = rotN alphaSize
